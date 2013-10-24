064d5c8f33b670876a27bb4e8dff08951c0e0fb8
TheMerovingian <bryan.quill@gmail.com>
2013-10-24 15:46:49 +1000

    Finished random rule generator.

--- LetterRecognitionDataMining/src/miningRules/RandomRuleGeneration.java
+++ LetterRecognitionDataMining/src/miningRules/RandomRuleGeneration.java
@@ -1,12 +1,38 @@
 package miningRules;
 
+import java.util.Random;
+
 public class RandomRuleGeneration implements RuleGenerator {
+
+	private final int RULE_LENGTH = Rule.RULE_LENGTH;
+	private final String CATEGORIES = Rule.CATEGORIES;
+	
+	private final int UPPER_BOUND = Rule.WINDOW_UPPER_BOUND + 2;
+	
+	private final int WILD_CARD = Rule.WILD_CARD;
+	
+	private Random random = new Random();
 
 	@Override
 	public Rule generateRule() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Rule rule = new Rule();
 
+		for (int i = 0; i < RULE_LENGTH; i++) {
+			int ruleVal = random.nextInt(UPPER_BOUND);
+			if (ruleVal > UPPER_BOUND - 2)
+				rule.setRuleValue(i, WILD_CARD);
+			else
+				rule.setRuleValue(i, ruleVal);
+		}
+		
+		int category = random.nextInt(CATEGORIES.length());
+		
+		if (category == CATEGORIES.length() - 1)
+			rule.setRuleCategory(CATEGORIES.substring(category));
+		else
+			rule.setRuleCategory(CATEGORIES.substring(category, category+1));
+		
+		return rule;
+	}
 
 }
--- LetterRecognitionDataMining/src/miningRules/Rule.java
+++ LetterRecognitionDataMining/src/miningRules/Rule.java
@@ -1,12 +1,61 @@
 package miningRules;
 
 public class Rule {
-	
+
+	public static final int RULE_LENGTH = 16;
+	public static final String CATEGORIES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static final int WINDOW_UPPER_BOUND = 15;
+	public static final int WINDOW_LOWER_BOUND = 0;
+	public static final int WILD_CARD = 255;
+
+	private int[] rule = new int[16];
+
+	private String category = "";
+
 	public int getSpecificity() {
-		return 0;
+		int specificity = 0;
+		for (int x : rule) {
+			if (x != WILD_CARD)
+				specificity++;
+		}
+		return specificity;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public boolean setRuleValue(int ruleIndex, int ruleValue) {
+		if (ruleValue == WILD_CARD) {
+			rule[ruleIndex] = ruleValue;
+			return true;
+		} else if (ruleValue < WINDOW_LOWER_BOUND
+				|| ruleValue > WINDOW_UPPER_BOUND)
+			return false;
+		else {
+			rule[ruleIndex] = ruleValue;
+			return true;
+		}
+	}
+
+	public boolean setRuleCategory(String category) {
+		if (CATEGORIES.contains(category)) {
+			this.category = category;
+			return true;
+		} else
+			return false;
 	}
 	
-	public String getResult() {
-		return "";
+	@Override
+	public String toString() {
+		
+		String output = "";
+		for (int val : rule)
+			output += "" + val + ",";
+		
+		output += category;
+		
+		return output;
 	}
 }
